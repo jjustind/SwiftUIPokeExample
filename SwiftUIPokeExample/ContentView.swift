ca640a998ef849881944de3d35ac41c02f8b86fd
Justin Davis <jdavis@trekk.com>
2019-07-03 16:00:13 -0500

    Initial Commit

---
+++ SwiftUIPokeExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIPokeExample
+//
+//  Created by Justin Davis on 7/3/19.
+//  Copyright © 2019 Trekk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
